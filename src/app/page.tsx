fc90f0d6220df7e98496cf64bbb968728eed130e
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-10 12:52:56 +0300

    Add Card component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import ContactSection from '../components/ContactSection';
 import Footer from '../components/Footer';
 import Button from '../components/Button';
 import SectionHeader from '../components/SectionHeader';
+import Card from '../components/Card';
 
 export default function HomePage() {
   return (
@@ -33,6 +34,7 @@ export default function HomePage() {
     <Footer />
     <Button />
     <SectionHeader />
+    <Card />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
