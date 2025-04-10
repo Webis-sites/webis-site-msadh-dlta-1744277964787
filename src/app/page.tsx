d6cf4ffc69afdf02e2d35b50f5a34175c07dc3b7
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-10 12:43:52 +0300

    Add ServicesSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import HeroSection from '../components/HeroSection';
 import NavigationBar from '../components/NavigationBar';
 import AboutSection from '../components/AboutSection';
 import FeaturedDishes from '../components/FeaturedDishes';
+import ServicesSection from '../components/ServicesSection';
 
 export default function HomePage() {
   return (
@@ -15,6 +16,7 @@ export default function HomePage() {
     <NavigationBar />
     <AboutSection />
     <FeaturedDishes />
+    <ServicesSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
