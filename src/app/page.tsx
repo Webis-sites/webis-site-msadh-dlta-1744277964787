812df84a1bf63de1c0a75136b4002d886944e5e3
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-10 12:42:26 +0300

    Add AboutSection component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import NavigationBar from '../components/NavigationBar';
+import AboutSection from '../components/AboutSection';
 
 export default function HomePage() {
   return (
@@ -11,6 +12,7 @@ export default function HomePage() {
         {/* כאן יתווספו הקומפוננטות שייווצרו על ידי המחולל */}
         <HeroSection />
     <NavigationBar />
+    <AboutSection />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
